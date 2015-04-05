f9ab144101fa16b83a21144f22160bfba9068db4
Leo An <leo@innoaus.com>
2015-04-05 09:58:55 +0900

    .

---
+++ devenv/build.sh
@@ -0,0 +1,14 @@
+cd icezmq
+
+wget https://dl.dropboxusercontent.com/u/57191386/zeromq-4.0.5-msvc2013-build-snapshot-20150404.zip
+wget https://dl.dropboxusercontent.com/u/57191386/ACE-6.3.1.zip
+wget https://dl.dropboxusercontent.com/u/57191386/Ice-3.5.1-3.msi
+
+git clone https://github.com/INNOAUS/icezmq.git
+cd ..
+tar cvfz icezma.tgz ./icezmq/*
+
+cd icezmq/icezmq
+
+make
+
