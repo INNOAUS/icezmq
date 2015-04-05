610f1bea44b944181bb86c8fa72eea905d0d894e
Leo An <leo@innoaus.com>
2015-04-05 10:06:23 +0900

    .

--- devenv/build.sh
+++ devenv/build.sh
@@ -1,8 +1,8 @@
 cd icezmq
 
-wget https://dl.dropboxusercontent.com/u/57191386/zeromq-4.0.5-msvc2013-build-snapshot-20150404.zip
-wget https://dl.dropboxusercontent.com/u/57191386/ACE-6.3.1.zip
-wget https://dl.dropboxusercontent.com/u/57191386/Ice-3.5.1-3.msi
+#wget https://dl.dropboxusercontent.com/u/57191386/zeromq-4.0.5-msvc2013-build-snapshot-20150404.zip
+#wget https://dl.dropboxusercontent.com/u/57191386/ACE-6.3.1.zip
+#wget https://dl.dropboxusercontent.com/u/57191386/Ice-3.5.1-3.msi
 
 git clone https://github.com/INNOAUS/icezmq.git
 cd ..
